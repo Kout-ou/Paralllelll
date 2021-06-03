07c1f3f20a9167a9c91b3bca682fa1614b1e92e1
Ioannis Loudaros <johnloudaros@hotmail.com>
2021-06-04 00:59:53 +0300

    Added Hooke Jeeves function and created main.c

---
+++ Code/Paralllelll/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Paralllelll
+//
+//  Created by Ioannis Loudaros on 4/6/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
